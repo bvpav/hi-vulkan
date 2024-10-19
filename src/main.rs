80f72cd32c257be8620ed69843545b2636fe83db
bvpav <78971592+bvpav@users.noreply.github.com>
2024-10-19 21:47:16 +0300

    struct in a buffer

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,57 @@
-use color_eyre::eyre::Result;
-use winit::application::ApplicationHandler;
-use winit::dpi::LogicalSize;
-use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowId};
+use std::sync::Arc;
+use std::default::Default;
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
+use vulkano::VulkanLibrary;
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
-
-#[derive(Default)]
-struct MyApp {
-    window: Option<Window>,
+#[derive(BufferContents)]
+#[repr(C)]
+struct MyStruct {
+    a: u32,
+    b: u32,
 }
 
-impl ApplicationHandler for MyApp {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        if let Ok(window) = event_loop.create_window(
-            Window::default_attributes()
-                .with_title("Hi, Vulkan")
-                .with_inner_size(LogicalSize::new(f64::from(WIDTH), f64::from(HEIGHT)))
-        ) {
-            // TODO: somehow handle error maybe??
-            self.window = Some(window);
-        }
-    }
+fn main() {
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .nth(1)
+        .expect("no Vulkan supported devices found");
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .position(|properties| {
+            properties.queue_flags.contains(QueueFlags::GRAPHICS)
+        })
+        .expect("could not find a graphical queue family") as u32;
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    ).expect("failed to create logical device");
+    let allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
-        match event {
-            WindowEvent::CloseRequested => {
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
-            }
-            _ => (),
-        }
-    }
-}
-
-fn main() -> Result<()> {
-    color_eyre::install()?;
-
-    let event_loop = EventLoop::new()?;
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = MyApp::default();
-
-    event_loop.run_app(&mut app)?;
-
-    Ok(())
+    let data = MyStruct { a: 69, b: 420 };
+    let _buffer = Buffer::from_data(
+        allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::UNIFORM_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        data,
+    ).expect("failed to create buffer");
 }
