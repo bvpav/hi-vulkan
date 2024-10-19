b939ba05b090f21cff1ff464c134cf9bb80f735a
bvpav <78971592+bvpav@users.noreply.github.com>
2024-10-19 23:22:33 +0300

    buffer memcpy

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use std::sync::Arc;
 use std::default::Default;
-use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
+use std::sync::Arc;
+
+use vulkano::{sync, VulkanLibrary};
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferInfo};
+use vulkano::command_buffer::allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo};
 use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
-use vulkano::VulkanLibrary;
-
-#[derive(BufferContents, Debug)]
-#[repr(C)]
-struct Thing {
-    number: u32,
-}
+use vulkano::sync::GpuFuture;
 
 fn main() {
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
@@ -27,7 +25,7 @@ fn main() {
             properties.queue_flags.contains(QueueFlags::GRAPHICS)
         })
         .expect("could not find a graphical queue family") as u32;
-    let (device, mut _queues) = Device::new(
+    let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo {
             queue_create_infos: vec![QueueCreateInfo {
@@ -37,27 +35,65 @@ fn main() {
             ..Default::default()
         },
     ).expect("failed to create logical device");
-    let allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+    let queue = queues.next().unwrap();
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
-    let iter = (0..128).map(|i| Thing { number: i * 2 });
-    let buffer = Buffer::from_iter(
-        allocator.clone(),
+    let source_content: Vec<i32> = (0..64).collect();
+    let source = Buffer::from_iter(
+        memory_allocator.clone(),
         BufferCreateInfo {
-            usage: BufferUsage::UNIFORM_BUFFER,
+            usage: BufferUsage::TRANSFER_SRC,
             ..Default::default()
         },
         AllocationCreateInfo {
-            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
                 | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
             ..Default::default()
         },
-        iter,
-    ).unwrap();
-    let content = buffer.read().unwrap();
-    let _ = dbg!(content.iter());
-    drop(content);
+        source_content,
+    ).expect("failed to allocate source buffer");
 
-    let mut content = buffer.write().unwrap();
-    content[1].number += 1;
-    dbg!(content);
+    let destination_content: Vec<i32> = (0..64).map(|_| 0).collect();
+    let destination = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        destination_content,
+    ).expect("failed to allocate destination buffer");
+
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+
+    let mut builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue_family_index,
+        CommandBufferUsage::OneTimeSubmit
+    ).unwrap();
+
+    builder
+        .copy_buffer(CopyBufferInfo::buffers(source.clone(), destination.clone()))
+        .unwrap();
+
+    let command_buffer = builder.build().unwrap();
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+    future.wait(None).unwrap();
+
+    let source_content = source.read().unwrap();
+    let destination_content = destination.read().unwrap();
+    assert_eq!(&*source_content, &*destination_content);
+
+    println!("all good");
 }
