8a1a4094e74c53bb3b668510071f44f990775541
bvpav <78971592+bvpav@users.noreply.github.com>
2024-10-20 20:37:31 +0300

    mandlebrot set

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::default::Default;
 use std::sync::Arc;
 
-use vulkano::{sync, VulkanLibrary};
+use image::{ImageBuffer, Rgba};
 use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
-use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage};
 use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
-use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CommandBufferUsage, CopyImageToBufferInfo,
+};
 use vulkano::descriptor_set::allocator::StandardDescriptorSetAllocator;
+use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
 use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::format::Format;
+use vulkano::image::view::ImageView;
+use vulkano::image::{Image, ImageCreateInfo, ImageType, ImageUsage};
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
-use vulkano::pipeline::{ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout, PipelineShaderStageCreateInfo};
 use vulkano::pipeline::compute::ComputePipelineCreateInfo;
 use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
+use vulkano::pipeline::{
+    ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout, PipelineShaderStageCreateInfo,
+};
 use vulkano::sync::GpuFuture;
+use vulkano::{sync, VulkanLibrary};
 
-mod cs {
+mod mandlebrot_shader {
     use vulkano_shaders::shader;
 
     shader! {
@@ -23,15 +31,29 @@ mod cs {
         src: r"
             #version 460
 
-            layout (local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+            layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-            layout(set = 0, binding = 0) buffer Data {
-                uint data[];
-            } buf;
+            layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
 
             void main() {
-                uint idx = gl_GlobalInvocationID.x;
-                buf.data[idx] *= 12;
+                vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+                vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+
+                vec2 z = vec2(0.0, 0.0);
+                float i;
+                for (i = 0.0; i < 1.0; i += 0.005) {
+                    z = vec2(
+                        z.x * z.x - z.y * z.y + c.x,
+                        z.y * z.x + z.x * z.y + c.y
+                    );
+
+                    if (length(z) > 4.0) {
+                        break;
+                    }
+                }
+
+                vec4 to_write = vec4(vec3(i), 1.0);
+                imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
             }
         ",
     }
@@ -39,7 +61,8 @@ mod cs {
 
 fn main() {
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
-    let instance = Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
     let physical_device = instance
         .enumerate_physical_devices()
         .expect("could not enumerate devices")
@@ -48,9 +71,7 @@ fn main() {
     let queue_family_index = physical_device
         .queue_family_properties()
         .iter()
-        .position(|properties| {
-            properties.queue_flags.contains(QueueFlags::GRAPHICS)
-        })
+        .position(|properties| properties.queue_flags.contains(QueueFlags::GRAPHICS))
         .expect("could not find a graphical queue family") as u32;
     let (device, mut queues) = Device::new(
         physical_device,
@@ -61,66 +82,80 @@ fn main() {
             }],
             ..Default::default()
         },
-    ).expect("failed to create logical device");
+    )
+    .expect("failed to create logical device");
     let queue = queues.next().unwrap();
     let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
-    let data_buffer = Buffer::from_iter(
+    let image = Image::new(
         memory_allocator.clone(),
-        BufferCreateInfo {
-            usage: BufferUsage::STORAGE_BUFFER,
+        ImageCreateInfo {
+            image_type: ImageType::Dim2d,
+            format: Format::R8G8B8A8_UNORM,
+            extent: [1024, 1024, 1],
+            usage: ImageUsage::STORAGE | ImageUsage::TRANSFER_SRC,
             ..Default::default()
         },
         AllocationCreateInfo {
-            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
-                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE,
             ..Default::default()
         },
-        0..65536u32,
-    ).expect("failed to create buffer");
+    )
+    .unwrap();
 
-    let shader_module = cs::load(device.clone())
-        .expect("failed to create shader module");
-    let entry_point = shader_module.entry_point("main").unwrap();
+    let image_view = ImageView::new_default(image.clone()).unwrap();
+
+    let shader = mandlebrot_shader::load(device.clone()).expect("failed to create shader module");
+    let entry_point = shader.entry_point("main").unwrap();
     let stage = PipelineShaderStageCreateInfo::new(entry_point);
-    let layout = PipelineLayout::new(
+    let pipeline_layout = PipelineLayout::new(
         device.clone(),
         PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
             .into_pipeline_layout_create_info(device.clone())
             .unwrap(),
-    ).unwrap();
+    )
+    .unwrap();
+
     let compute_pipeline = ComputePipeline::new(
         device.clone(),
         None,
-        ComputePipelineCreateInfo::stage_layout(stage, layout),
-    ).expect("failed to create compute pipeline");
-
-    let descriptor_set_allocator = StandardDescriptorSetAllocator::new(device.clone(), Default::default());
-    let pipeline_layout = compute_pipeline.layout();
-    let descriptor_set_layouts = pipeline_layout.set_layouts();
-
-    let descriptor_set_layout_index = 0;
-    let descriptor_set_layout = descriptor_set_layouts
-        .get(descriptor_set_layout_index)
-        .unwrap();
+        ComputePipelineCreateInfo::stage_layout(stage, pipeline_layout),
+    )
+    .expect("failed to create compute pipeline");
+    let descriptor_set_layout = compute_pipeline.layout().set_layouts().first().unwrap();
+    let descriptor_set_allocator =
+        StandardDescriptorSetAllocator::new(device.clone(), Default::default());
     let descriptor_set = PersistentDescriptorSet::new(
         &descriptor_set_allocator,
         descriptor_set_layout.clone(),
-        [WriteDescriptorSet::buffer(0, data_buffer.clone())],
+        [WriteDescriptorSet::image_view(0, image_view.clone())],
         [],
-    ).unwrap();
+    )
+    .unwrap();
 
-    let command_buffer_allocator = StandardCommandBufferAllocator::new(
-        device.clone(),
-        Default::default(),
-    );
+    let buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        (0..1024 * 1024 * 4).map(|_| 0u8),
+    )
+    .expect("failed to allocate buffer");
+
+    let command_buffer_allocator =
+        StandardCommandBufferAllocator::new(device.clone(), Default::default());
     let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
         &command_buffer_allocator,
         queue.queue_family_index(),
         CommandBufferUsage::OneTimeSubmit,
-    ).unwrap();
-
-    let work_group_counts = [1024, 1, 1];
+    )
+    .unwrap();
 
     command_buffer_builder
         .bind_pipeline_compute(compute_pipeline.clone())
@@ -128,11 +163,16 @@ fn main() {
         .bind_descriptor_sets(
             PipelineBindPoint::Compute,
             compute_pipeline.layout().clone(),
-            descriptor_set_layout_index as u32,
+            0,
             descriptor_set,
         )
         .unwrap()
-        .dispatch(work_group_counts)
+        .dispatch([1024 / 8, 1024 / 8, 1])
+        .unwrap()
+        .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
+            image.clone(),
+            buffer.clone(),
+        ))
         .unwrap();
 
     let command_buffer = command_buffer_builder.build().unwrap();
@@ -144,10 +184,9 @@ fn main() {
         .unwrap();
     future.wait(None).unwrap();
 
-    let content = data_buffer.read().unwrap();
-    for (n, val) in content.iter().enumerate() {
-        assert_eq!(*val, n as u32 * 12);
-    }
+    let buffer_content = buffer.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("image.png").unwrap();
 
-    println!("we survived!");
+    println!("yoo we done");
 }
